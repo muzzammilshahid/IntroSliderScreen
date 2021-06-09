d1a63352c48293c6c4e9b64b83b5d0d5af766876
Muzammil Shahid <muzzammil.shahid.311@gmail.com>
2021-06-09 16:25:52 +0500

    initial commit

---
+++ app/src/main/java/com/example/introslidersscreen/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.introslidersscreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        PrefManager prefManager = new PrefManager(getApplicationContext());
+        if(prefManager.isFirstTimeLaunch()){
+            prefManager.setFirstTimeLaunch(false);
+            startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
+            finish();
+        }
+
+        findViewById(R.id.btn_click).setOnClickListener(v -> Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show());
+    }
+
+}
